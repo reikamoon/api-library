a76c6d56212315bcef3ee019ffa6748c359ee7ed
Anjelica <ambrosio.anjelica@gmail.com>
2021-04-21 16:18:16 -0700

    finished challenge 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
  // Functions 
-    function getWeather(zip, apiKey, successCallback, units = 'imperial') {
+    async function getWeather(zip, apiKey, units = 'imperial') {
         const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
         
-        const resPromise = fetch(path)
-        const jsonPromise = resPromise.then(res => res.json())
-        return jsonPromise
-      }
+        try {
+            const res = await fetch(path)
+            const json = await res.json()
+            return json
+        } catch(err) {
+            return err
+        }
+        }
 
       export {
           getWeather
