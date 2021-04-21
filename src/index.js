ff1c00583524e854f82361f2ca8044e306cccc0b
Anjelica <ambrosio.anjelica@gmail.com>
2021-04-21 16:09:03 -0700

    finished two challenges

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
  // Functions 
-    function getWeather(zip, successCallback) {
-        const apiKey = '467355df4c808dd6134a3b64e9ace282'
-        const units = 'imperial'
+    function getWeather(zip, apiKey, successCallback) {
         const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
         fetch(path)
           .then(res => res.json())
-          .then(json => {
-            successCallback(json)
-          })
+          .then(json => successCallback(json))
           .catch(err => console.log(err.message))
       }
 
