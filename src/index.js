7031d196576fcf212b34aab904122b716f83d28c
Anjelica <ambrosio.anjelica@gmail.com>
2021-04-21 16:20:53 -0700

    completed challenge 5

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
         try {
             const res = await fetch(path)
             const json = await res.json()
-            return json
+            console.log(json)
+            const weatherData = {
+                code: json.cod,
+                coord: json.coord,
+                temp: json.main.temp,
+                feels_like: json.main.feels_like,
+                temp_min: json.main.temp_min,
+                temp_max: json.main.temp_max,
+                description: json.weather[0].description
+            }
+            return weatherData
         } catch(err) {
             return err
         }
